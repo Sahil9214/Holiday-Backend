5d0c7b3e18b305eb81e069ac600ea2b458f40241
Utkarsh <utkarshsinghal369@gmail.com>
2024-06-06 18:26:15 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const { connection } = require("./config/db");
 const { employeeRouter } = require("./routes/employeesroute");
 const PORT = process.env.PORT || 3000;
+var cors = require("cors");
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 // Routes
 app.use("/api", employeeRouter);
 app.listen(process.env.PORT, async (req, res) => {
